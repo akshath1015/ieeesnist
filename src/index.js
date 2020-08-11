b3c7bc4f054ca75425201e33331541485aa06d2c
akshath1015 <akshath1015@gmail.com>
2020-08-11 12:52:12 +0530

    Mobile Version Changes

--- src/index.js
+++ src/index.js
@@ -137,7 +137,7 @@ app.get('/construction', (req,res) => {
 })
 
 //Listening to the Server
-const port = process.env.PORT || 8051
+const port = process.env.PORT || 8054
 app.listen(port,() => {
     console.log('Server Running on PORT:' + port)
 })
