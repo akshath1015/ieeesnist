8a42ddb18da5c622d2089c1f738002a3f89234b4
akshath1015 <akshath1015@gmail.com>
2020-09-13 09:47:55 +0530

    committed

--- src/index.js
+++ src/index.js
@@ -137,7 +137,7 @@ app.get('/construction', (req,res) => {
 })
 
 //Listening to the Server
-const port = process.env.PORT || 8054
+const port = process.env.PORT || 8055
 app.listen(port,() => {
     console.log('Server Running on PORT:' + port)
 })
